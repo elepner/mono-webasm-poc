1b24d634d945a83da852fbb31e692b80e04c0de9
Eduard Lepner <eduard@invarion.com>
2020-12-02 12:31:17 +0100

    Add project files.

---
+++ MyLibrary/Foo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+
+namespace MyLibrary
+{
+    public static class Foo
+    {
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static string EnrichObject(string jsonString)
+        {
+            var jObject = JObject.Parse(jsonString);
+            jObject["Foo"] = "Bar";
+            return jObject.ToString();
+        }
+    }
+}
---
+++ TestingMonowasm/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using MyLibrary;
+
+namespace TestingMonowasm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Foo.Sum(420, 69));
+            Console.WriteLine(Foo.EnrichObject("{test:'Test'}"));
+        }
+    }
+}
